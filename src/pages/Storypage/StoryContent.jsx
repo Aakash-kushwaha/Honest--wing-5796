5db21dd09298f71f00effed052f1024f96cd679a
Suhani1102 <sandhusuhani1102@gmail.com>
2022-07-20 18:38:50 +0530

    added description & footer

--- src/pages/Storypage/StoryContent.jsx
+++ src/pages/Storypage/StoryContent.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-import { Box, Image, Heading, Text } from "@chakra-ui/react";
+import { Box, Image, Heading, Text, Icon, HStack } from "@chakra-ui/react";
 
 const StoryContent = () => {
+  const CircleIcon = (props) => (
+    <Icon viewBox="0 0 200 200" {...props}>
+      <path
+        fill="currentColor"
+        d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
+      />
+    </Icon>
+  );
+
+  const FooterTitles = [
+    "Terms of Use",
+    "Privacy Policy",
+    "Interest-Based Ads",
+    "Addendum to the Global Privacy Policy",
+    "Feedback",
+  ];
   return (
     <Box
       border="1px"
@@ -15,10 +31,79 @@ const StoryContent = () => {
         Story Title
       </Heading>
       <Text fontSize="xl">Story sub-title</Text>
+
       <Image
         borderRadius="0.75rem"
         src="https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_1280,q_70/lsci/db/PICTURES/CMS/341700/341768.4.jpg"
       />
+      <Text p="0 15%" mt="2rem">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Cursus risus at
+        ultrices mi tempus imperdiet. Imperdiet dui accumsan sit amet nulla.
+        Sollicitudin tempor id eu nisl nunc mi ipsum. Urna molestie at elementum
+        eu facilisis sed odio morbi quis. Et odio pellentesque diam volutpat
+        commodo. Malesuada pellentesque elit eget gravida cum sociis natoque.
+        Aliquam id diam maecenas ultricies mi eget mauris. Vitae congue mauris
+        rhoncus aenean vel elit scelerisque mauris pellentesque. In tellus
+        integer feugiat scelerisque varius morbi enim nunc faucibus. In dictum
+        non consectetur a erat. Tempus iaculis urna id volutpat lacus laoreet
+        non. Aliquam faucibus purus in massa. Integer quis auctor elit sed
+        vulputate mi. Volutpat consequat mauris nunc congue nisi vitae suscipit
+        tellus mauris. Integer enim neque volutpat ac tincidunt vitae semper
+        quis. Dolor purus non enim praesent. Quam pellentesque nec nam aliquam
+        sem et tortor consequat id. Turpis egestas maecenas pharetra convallis
+        posuere. Ut aliquam purus sit amet luctus. Ut tortor pretium viverra
+        suspendisse potenti nullam ac. Vivamus at augue eget arcu dictum varius
+        duis at. Arcu odio ut sem nulla pharetra diam sit amet nisl. Turpis nunc
+        eget lorem dolor sed viverra. Laoreet non curabitur gravida arcu ac
+        tortor. Tristique senectus et netus et. Feugiat sed lectus vestibulum
+        mattis ullamcorper velit. Orci phasellus egestas tellus rutrum tellus.
+        Pharetra et ultrices neque ornare aenean euismod. Sed adipiscing diam
+        donec adipiscing tristique risus nec feugiat. Egestas purus viverra
+        accumsan in nisl. Congue nisi vitae suscipit tellus mauris a diam
+        maecenas. Mi proin sed libero enim. Eget nullam non nisi est. Consequat
+        id porta nibh venenatis. Pharetra massa massa ultricies mi quis.
+        Condimentum mattis pellentesque id nibh tortor. Odio morbi quis commodo
+        odio aenean sed adipiscing diam. Eget aliquet nibh praesent tristique
+        magna sit amet purus gravida. Vulputate mi sit amet mauris. Elit
+        ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Nibh
+        ipsum consequat nisl vel. Sed egestas egestas fringilla phasellus
+        faucibus. Volutpat ac tincidunt vitae semper quis lectus. Tincidunt eget
+        nullam non nisi est sit amet facilisis. Urna porttitor rhoncus dolor
+        purus non enim praesent. Cras sed felis eget velit aliquet sagittis.
+        Urna condimentum mattis pellentesque id nibh tortor. Etiam non quam
+        lacus suspendisse faucibus interdum posuere. Feugiat nisl pretium fusce
+        id velit ut. Quam pellentesque nec nam aliquam sem et tortor consequat
+        id. Urna porttitor rhoncus dolor purus. Nulla facilisi etiam dignissim
+        diam quis enim lobortis scelerisque fermentum. Eu turpis egestas pretium
+        aenean. Dolor magna eget est lorem ipsum dolor. Tincidunt vitae semper
+        quis lectus nulla. Vitae turpis massa sed elementum tempus egestas sed
+        sed risus. Arcu risus quis varius quam quisque id diam vel. At ultrices
+        mi tempus imperdiet nulla. Odio aenean sed adipiscing diam donec
+        adipiscing tristique risus. Sollicitudin aliquam ultrices sagittis orci
+        a. Suspendisse in est ante in nibh mauris cursus mattis. Ultrices in
+        iaculis nunc sed. Quam adipiscing vitae proin sagittis nisl rhoncus
+        mattis rhoncus urna. Scelerisque varius morbi enim nunc. Euismod in
+        pellentesque massa placerat. Magna eget est lorem ipsum. Posuere lorem
+        ipsum dolor sit amet consectetur adipiscing elit duis. Et odio
+        pellentesque diam volutpat commodo sed egestas. Purus gravida quis
+        blandit turpis cursus in hac habitasse. Id aliquet lectus proin nibh
+        nisl condimentum.
+      </Text>
+      <HStack fontSize="0.75rem" mt="1rem">
+        {FooterTitles.map((item, index) => {
+          if (index === FooterTitles.length - 1) {
+            return <Text>{item}</Text>;
+          } else {
+            return (
+              <>
+                <Text>{item}</Text>
+                <CircleIcon boxSize={1} />
+              </>
+            );
+          }
+        })}
+      </HStack>
     </Box>
   );
 };
