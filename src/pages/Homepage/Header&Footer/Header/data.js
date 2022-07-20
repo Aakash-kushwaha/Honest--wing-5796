a669b209af752e678df5f0c503c10bc390762794
Aakash Kushwaha <99542364+Aakash-kushwaha@users.noreply.github.com>
2022-07-20 20:41:48 +0530

    slider complete

---
+++ src/pages/Homepage/Header&Footer/Header/data.js
@@ -0,0 +1,623 @@
+export const data=  [
+    {
+    "id": "5c92585f-f619-4199-9a29-9bd61cad4006",
+    "name": "Salem Spartans vs Chepauk Super Gillies, 22nd Match",
+    "status": "Chepauk Super Gillies won by 7 wkts",
+    "venue": "SCF Cricket Ground, Salem",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T13:45:00",
+    "teams": [
+    "Salem Spartans",
+    "Chepauk Super Gillies"
+    ],
+    "teamInfo": [
+    {
+    "name": "Chepauk Super Gillies",
+    "shortname": "CSG",
+    "img": "https://cdorgapi.b-cdn.net/img/icon512.png"
+    },
+    {
+    "name": "Salem Spartans",
+    "shortname": "SLST",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/267-637937642932634596.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 113,
+    "w": 6,
+    "o": 20,
+    "inning": "Salem Spartans Inning 1"
+    },
+    {
+    "r": 117,
+    "w": 3,
+    "o": 14.5,
+    "inning": "Chepauk Super Gillies Inning 1"
+    }
+    ],
+    "series_id": "41bcbe72-ee4b-454c-90eb-fc68798e383a",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "678a489c-2f73-49dd-adcc-da55bc3b237e",
+    "name": "Ireland Women vs Pakistan Women, 3rd Match",
+    "status": "Pakistan Women won by 13 runs (DLS method) - Reduced to 14 overs, target 97 runs",
+    "venue": "Bready Cricket Club, Bready, Northern Ireland",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T11:00:00",
+    "teams": [
+    "Ireland Women",
+    "Pakistan Women"
+    ],
+    "teamInfo": [
+    {
+    "name": "Ireland Women",
+    "shortname": "IRW",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1420-637926315409640205.png"
+    },
+    {
+    "name": "Pakistan Women",
+    "shortname": "PAKW",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/67-637877074931980375.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 92,
+    "w": 5,
+    "o": 14,
+    "inning": "Pakistan Women Inning 1"
+    },
+    {
+    "r": 83,
+    "w": 6,
+    "o": 14,
+    "inning": "Ireland Women Inning 1"
+    }
+    ],
+    "series_id": "0d8492b4-003d-4b06-8b40-752cec7c8441",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "18f3488a-3778-462a-87f7-a30d6cb46533",
+    "name": "Italy vs Isle of Man, Final",
+    "status": "Italy won by 7 wkts",
+    "venue": "Kerava National Cricket Ground, Kerava",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T13:00:00",
+    "teams": [
+    "Italy",
+    "Isle of Man"
+    ],
+    "teamInfo": [
+    {
+    "name": "Isle of Man",
+    "shortname": "IOM",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/616-637928917956919054.png"
+    },
+    {
+    "name": "Italy",
+    "shortname": "ITA",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/617-637877089742553367.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 101,
+    "w": 8,
+    "o": 20,
+    "inning": "Isle of Man Inning 1"
+    },
+    {
+    "r": 102,
+    "w": 3,
+    "o": 10.5,
+    "inning": "Italy Inning 1"
+    }
+    ],
+    "series_id": "a4d4e28c-eb51-4d4b-9b5d-1f69bd6dfe66",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "6bfdb510-c038-4464-ad83-12129e8c8817",
+    "name": "Sweden vs Romania, 5th Place Play-off",
+    "status": "Sweden won by 8 wkts",
+    "venue": "Tikkurila Cricket Ground, Vantaa",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T13:00:00",
+    "teams": [
+    "Sweden",
+    "Romania"
+    ],
+    "teamInfo": [
+    {
+    "name": "Romania",
+    "shortname": "ROM",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/645-637867439654876721.png"
+    },
+    {
+    "name": "Sweden",
+    "shortname": "SWE",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/659-637869111698226088.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 104,
+    "w": 10,
+    "o": 19.5,
+    "inning": "Romania Inning 1"
+    },
+    {
+    "r": 106,
+    "w": 2,
+    "o": 14.2,
+    "inning": "Sweden Inning 1"
+    }
+    ],
+    "series_id": "a4d4e28c-eb51-4d4b-9b5d-1f69bd6dfe66",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "a4a17d4c-2da3-4f5e-ba1f-fe0a50d4efc8",
+    "name": "Finland vs Cyprus, 3rd Place Play-off",
+    "status": "Finland won by 11 runs",
+    "venue": "Kerava National Cricket Ground, Kerava",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T08:00:00",
+    "teams": [
+    "Finland",
+    "Cyprus"
+    ],
+    "teamInfo": [
+    {
+    "name": "Cyprus",
+    "shortname": "CYP",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/597-637872833850554962.webp"
+    },
+    {
+    "name": "Finland",
+    "shortname": "FIN",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/604-637871098066577941.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 159,
+    "w": 10,
+    "o": 19.3,
+    "inning": "Finland Inning 1"
+    },
+    {
+    "r": 148,
+    "w": 7,
+    "o": 20,
+    "inning": "Cyprus Inning 1"
+    }
+    ],
+    "series_id": "a4d4e28c-eb51-4d4b-9b5d-1f69bd6dfe66",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "95e0548e-6c8a-4ec8-92d5-1be891e75bef",
+    "name": "Croatia vs Serbia, 7th Place Play-off",
+    "status": "Croatia won by 3 wkts",
+    "venue": "Tikkurila Cricket Ground, Vantaa",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T08:00:00",
+    "teams": [
+    "Croatia",
+    "Serbia"
+    ],
+    "teamInfo": [
+    {
+    "name": "Croatia",
+    "shortname": "CRT",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1773-637935033567722609.webp"
+    },
+    {
+    "name": "Serbia",
+    "shortname": "SRB",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/651-637876983315009937.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 90,
+    "w": 10,
+    "o": 20,
+    "inning": "Serbia Inning 1"
+    },
+    {
+    "r": 94,
+    "w": 7,
+    "o": 18.4,
+    "inning": "Croatia Inning 1"
+    }
+    ],
+    "series_id": "a4d4e28c-eb51-4d4b-9b5d-1f69bd6dfe66",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "6ecf69be-341a-4913-ba11-a4a9cf752655",
+    "name": "Sri Lanka vs Pakistan, 1st Test",
+    "matchType": "test",
+    "status": "Day 5: 2nd Session - Pakistan won by 4 wkts",
+    "venue": "Galle International Stadium, Galle",
+    "date": "2022-07-16",
+    "dateTimeGMT": "2022-07-16T04:30:00",
+    "teams": [
+    "Sri Lanka",
+    "Pakistan"
+    ],
+    "teamInfo": [
+    {
+    "name": "Pakistan",
+    "shortname": "PAK",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/66-637877075103236690.webp"
+    },
+    {
+    "name": "Sri Lanka",
+    "shortname": "SL",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/86-637877070226216908.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 222,
+    "w": 10,
+    "o": 66.1,
+    "inning": "Sri Lanka Inning 1"
+    },
+    {
+    "r": 218,
+    "w": 10,
+    "o": 90.5,
+    "inning": "Pakistan Inning 1"
+    },
+    {
+    "r": 337,
+    "w": 10,
+    "o": 100,
+    "inning": "Sri Lanka Inning 2"
+    },
+    {
+    "r": 342,
+    "w": 6,
+    "o": 127.2,
+    "inning": "Pakistan Inning 2"
+    }
+    ],
+    "series_id": "9ddc7495-f650-4526-b1ed-f5e7ee149bed",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "a9b01826-ab8f-4ae4-943e-a020fa9ffd50",
+    "name": "Middlesex vs Sussex, County Div 2",
+    "matchType": "test",
+    "status": "Day 1: Stumps - Middlesex opt to bowl",
+    "venue": "Lord's, London",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T10:00:00",
+    "teams": [
+    "Middlesex",
+    "Sussex"
+    ],
+    "teamInfo": [
+    {
+    "name": "Middlesex",
+    "shortname": "MDX",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1127-637889091340507643.webp"
+    },
+    {
+    "name": "Sussex",
+    "shortname": "SUSS",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1134-637889091814592518.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 328,
+    "w": 4,
+    "o": 96,
+    "inning": "Sussex Inning 1"
+    }
+    ],
+    "series_id": "75e0571e-958f-474e-b772-5665980e3d86",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "75903f75-949a-44a3-b911-ec9c597d1b5c",
+    "name": "Derbyshire vs Nottinghamshire, County Div 2",
+    "matchType": "test",
+    "status": "Day 1: Stumps - Nottinghamshire opt to bat",
+    "venue": "County Ground, Derby",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T10:00:00",
+    "teams": [
+    "Derbyshire",
+    "Nottinghamshire"
+    ],
+    "teamInfo": [
+    {
+    "name": "Derbyshire",
+    "shortname": "DERB",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1117-637889091569915358.webp"
+    },
+    {
+    "name": "Nottinghamshire",
+    "shortname": "NOT",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1130-637889092060873937.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 439,
+    "w": 1,
+    "o": 96,
+    "inning": "Nottinghamshire Inning 1"
+    }
+    ],
+    "series_id": "75e0571e-958f-474e-b772-5665980e3d86",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "5bfab9f8-5356-4c49-991a-c2efb6c5856b",
+    "name": "Gloucestershire vs Hampshire, County Div 1",
+    "matchType": "test",
+    "status": "Day 1: Stumps - Hampshire opt to bat",
+    "venue": "College Ground, Cheltenham",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T10:00:00",
+    "teams": [
+    "Gloucestershire",
+    "Hampshire"
+    ],
+    "teamInfo": [
+    {
+    "name": "Gloucestershire",
+    "shortname": "GLOU",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1121-637885550747990018.webp"
+    },
+    {
+    "name": "Hampshire",
+    "shortname": "HAM",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1122-637885550993657486.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 203,
+    "w": 2,
+    "o": 72,
+    "inning": "Hampshire Inning 1"
+    }
+    ],
+    "series_id": "87df720a-ee24-4e40-9448-98f000fe6748",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "192f9d33-16b9-479c-ba9f-26ee8c8fa1d0",
+    "name": "Northamptonshire vs Lancashire, County Div 1",
+    "matchType": "test",
+    "status": "Day 1: Stumps - Northamptonshire opt to bat",
+    "venue": "County Ground, Northampton",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T10:00:00",
+    "teams": [
+    "Northamptonshire",
+    "Lancashire"
+    ],
+    "teamInfo": [
+    {
+    "name": "Lancashire",
+    "shortname": "LNCS",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1125-637885551497331567.webp"
+    },
+    {
+    "name": "Northamptonshire",
+    "shortname": "NOR",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1129-637885551856435418.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 218,
+    "w": 7,
+    "o": 72,
+    "inning": "Northamptonshire Inning 1"
+    }
+    ],
+    "series_id": "87df720a-ee24-4e40-9448-98f000fe6748",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "e39e549a-f5a9-4169-8a01-2beb81ed7d5f",
+    "name": "Somerset vs Yorkshire, County Div 1",
+    "matchType": "test",
+    "status": "Day 1: Stumps - Somerset opt to bat",
+    "venue": "The Cooper Associates County Ground, Taunton",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T10:00:00",
+    "teams": [
+    "Somerset",
+    "Yorkshire"
+    ],
+    "teamInfo": [
+    {
+    "name": "Somerset",
+    "shortname": "SOM",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1131-637885552070524344.webp"
+    },
+    {
+    "name": "Yorkshire",
+    "shortname": "YRK",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1139-637885552795894277.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 262,
+    "w": 5,
+    "o": 76,
+    "inning": "Somerset Inning 1"
+    }
+    ],
+    "series_id": "87df720a-ee24-4e40-9448-98f000fe6748",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "9d730cd5-c536-4a15-8609-bd09e755c659",
+    "name": "Surrey vs Essex, County Div 1",
+    "matchType": "test",
+    "status": "Day 1: Stumps - Surrey trail by 247 runs",
+    "venue": "Kennington Oval, London",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T10:00:00",
+    "teams": [
+    "Surrey",
+    "Essex"
+    ],
+    "teamInfo": [
+    {
+    "name": "Essex",
+    "shortname": "ESX",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1119-637885549753088458.webp"
+    },
+    {
+    "name": "Surrey",
+    "shortname": "SUR",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1133-637885552361099530.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 271,
+    "w": 10,
+    "o": 84,
+    "inning": "Essex Inning 1"
+    },
+    {
+    "r": 24,
+    "w": 1,
+    "o": 9.1,
+    "inning": "Surrey Inning 1"
+    }
+    ],
+    "series_id": "87df720a-ee24-4e40-9448-98f000fe6748",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "c0a4c7ba-f782-4334-baff-37da24bf1245",
+    "name": "Warwickshire vs Kent, County Div 1",
+    "matchType": "test",
+    "status": "Day 1: Stumps - Warwickshire trail by 10 runs",
+    "venue": "Edgbaston, Birmingham",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T10:00:00",
+    "teams": [
+    "Warwickshire",
+    "Kent"
+    ],
+    "teamInfo": [
+    {
+    "name": "Kent",
+    "shortname": "KENT",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1124-637885551212175895.webp"
+    },
+    {
+    "name": "Warwickshire",
+    "shortname": "WRKS",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/1137-637885552592685258.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 165,
+    "w": 10,
+    "o": 40.1,
+    "inning": "Kent Inning 1"
+    },
+    {
+    "r": 155,
+    "w": 6,
+    "o": 53,
+    "inning": "Warwickshire Inning 1"
+    }
+    ],
+    "series_id": "87df720a-ee24-4e40-9448-98f000fe6748",
+    "fantasyEnabled": true,
+    "bbbEnabled": false,
+    "hasSquad": true
+    },
+    {
+    "id": "2a201c58-f701-4f66-95f2-78d246451390",
+    "name": "England vs South Africa, 1st ODI",
+    "matchType": "odi",
+    "status": "South Africa won by 62 runs",
+    "venue": "Riverside Ground, Chester-le-Street",
+    "date": "2022-07-19",
+    "dateTimeGMT": "2022-07-19T12:00:00",
+    "teams": [
+    "England",
+    "South Africa"
+    ],
+    "teamInfo": [
+    {
+    "name": "England",
+    "shortname": "ENG",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/23-637877072894080569.webp"
+    },
+    {
+    "name": "South Africa",
+    "shortname": "RSA",
+    "img": "https://cdorgapi.b-cdn.net/img/teams/82-637877071839088912.webp"
+    }
+    ],
+    "score": [
+    {
+    "r": 333,
+    "w": 5,
+    "o": 50,
+    "inning": "South Africa Inning 1"
+    },
+    {
+    "r": 271,
+    "w": 10,
+    "o": 46.5,
+    "inning": "England Inning 1"
+    }
+    ],
+    "series_id": "a6e31dc8-8cae-4fb1-854f-d3a94cdc301d",
+    "fantasyEnabled": true,
+    "bbbEnabled": true,
+    "hasSquad": true
+    }
+    ]
