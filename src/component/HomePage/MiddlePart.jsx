b14b6a006e0b0eaaecc28c344b96238a779df179
Dhruba Singha <97454787+Dhruba1010@users.noreply.github.com>
2022-07-20 21:18:45 +0530

    done

--- src/component/HomePage/MiddlePart.jsx
+++ src/component/HomePage/MiddlePart.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
+import NewsCard from './NewsCard';
 
 const MiddlePart = () => {
   return (
-    <Box border='1px solid green' borderRadius='lg' w='51%'>
-      MiddlePart
+    <Box border='1px solid green' borderRadius='lg' w='51%' >
+      <NewsCard />
     </Box>
   )
 }
