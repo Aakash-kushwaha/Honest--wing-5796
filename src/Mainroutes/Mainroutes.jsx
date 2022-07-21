121b01748c1edd2eed6f003c52e8a540d145a231
Vikashwar <93377543+Vikashwar@users.noreply.github.com>
2022-07-21 22:19:00 +0530

    series

--- src/Mainroutes/Mainroutes.jsx
+++ src/Mainroutes/Mainroutes.jsx
@@ -4,7 +4,7 @@ import SimpleSlider from "../pages/Homepage/Header&Footer/Header/Slider";
 import Homepage from "../pages/Homepage/Homepage";
 import MainNews from "../pages/Newspage/MainNews";
 import Storypage from "../pages/Storypage/Storypage";
-
+import Scoredescription from "../pages/Scoredescription/Scoredescription";
 const Mainroutes = () => {
   return (
       
@@ -12,7 +12,7 @@ const Mainroutes = () => {
     <Route path={"/slider"} element={<SimpleSlider></SimpleSlider>}></Route>
     <Route path="/" element={<Homepage></Homepage>}></Route>
     <Route path={"/newspage"} element={<MainNews></MainNews>}></Route>
-     
+    <Route path={"/Scoredescription"} element={<Scoredescription></Scoredescription>}></Route>
       <Route path="/storypage" element={<Storypage />} />
     </Routes>
   );
