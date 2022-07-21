e48755c6388e77330dc7ac2fa2cc98bcd98ed7f5
Suhani1102 <sandhusuhani1102@gmail.com>
2022-07-21 18:30:45 +0530

    left bar updated

--- src/pages/Storypage/StoryNavigationLeft.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
-
-const StoryNavigationLeft = () => {
-  return <Box>Story Navigation Left</Box>;
-};
-
-export default StoryNavigationLeft;
