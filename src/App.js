f11a13e80e4e9e406dd782167461b44014c82560
Suhani1102 <sandhusuhani1102@gmail.com>
2022-07-22 12:40:04 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import { useContext, useEffect } from "react";
-import axios from "axios";
 import Homepage from "./pages/Homepage/Homepage";
 import { Box } from "@chakra-ui/react";
 
@@ -8,22 +6,8 @@ import logo from "./logo.svg";
 // import './App.css';
 import SimpleSlider from "./pages/Homepage/Header&Footer/Header/Slider";
 import Mainroutes from "./Mainroutes/Mainroutes";
-import { NewsContext } from "./context/NewsContext";
 
 function App() {
-  const { setNews } = useContext(NewsContext);
-
-  useEffect(() => {
-    const key = "e7872f048197450d858f90869108d99a";
-    // let getData = async () => {
-    //   let r = await axios.get(
-    //     `https://newsapi.org/v2/everything?q=Cricket&from=2022-07-21&sortBy=popularity&apiKey=${key}`
-    //   );
-    //   setNews(r.data);
-    //   console.log("Appjs ", r.data);
-    // };
-    // getData();
-  }, []);
   return (
     <div className="App">
       <Mainroutes></Mainroutes>
