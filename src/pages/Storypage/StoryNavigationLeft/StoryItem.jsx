2f625da0dc4433d9b95ca00857d13feb502a2a27
Suhani1102 <sandhusuhani1102@gmail.com>
2022-07-22 13:42:10 +0530

    updated left panel navigation

--- src/pages/Storypage/StoryNavigationLeft/StoryItem.jsx
+++ src/pages/Storypage/StoryNavigationLeft/StoryItem.jsx
@@ -1,24 +1,30 @@
 import React, { Fragment } from "react";
 import { Box, Heading, Text } from "@chakra-ui/react";
 import CircleIcon from "../../../component/CircleIcon/CircleIcon";
+import { Link as RouterLink } from "react-router-dom";
 
 const StoryItem = (props) => {
   const news = props.data;
+
   return (
     <Fragment>
-      <Heading as="h6" size="xs">
-        {news.title}
-      </Heading>
-      <Box display={"flex"} fontSize="0.7rem">
-        <Text color={"rgba(72,73,74)"}>{news.id % 2 === 0 ? "4hr" : "1d"}</Text>
-        <CircleIcon
-          boxSize={2}
-          height={"auto"}
-          color={"rgba(203,204,206)"}
-          m={"0 5px"}
-        />
-        <Text color={"rgba(72,73,74)"}>{news.author}</Text>
-      </Box>
+      <RouterLink to={`/storypage/${news.id}`}>
+        <Heading as="h6" size="xs">
+          {news.title}
+        </Heading>
+        <Box display={"flex"} fontSize="0.7rem">
+          <Text color={"rgba(72,73,74)"}>
+            {news.id % 2 === 0 ? "4hr" : "1d"}
+          </Text>
+          <CircleIcon
+            boxSize={2}
+            height={"auto"}
+            color={"rgba(203,204,206)"}
+            m={"0 5px"}
+          />
+          <Text color={"rgba(72,73,74)"}>{news.author}</Text>
+        </Box>
+      </RouterLink>
     </Fragment>
   );
 };
--- src/pages/Storypage/StoryNavigationLeft/StoryNavigationLeft.jsx
+++ src/pages/Storypage/StoryNavigationLeft/StoryNavigationLeft.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
 import StoryItem from "./StoryItem";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const StoryNavigationLeft = () => {
   const [news, setNews] = useState([]);
+  const { id } = useParams();
 
   const getData = () => {
     axios
@@ -25,8 +27,12 @@ const StoryNavigationLeft = () => {
     <Box mt="2rem">
       {news.map((news) => {
         return (
-          <Box p="1rem" key={news.id}>
-            <StoryItem data={news} />
+          <Box
+            p="1rem"
+            key={news.id}
+            borderLeft={news.id === Number(id) ? "3px solid #03a9f4" : "none"}
+          >
+            <StoryItem key={news.id} data={news} />
           </Box>
         );
       })}
