13e65eae283078ff085618d283117d6437e4677c
Suhani1102 <sandhusuhani1102@gmail.com>
2022-07-22 23:36:23 +0530

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
 import { Box } from "@chakra-ui/react";
-
-import logo from "./logo.svg";
-// import './App.css';
 import SimpleSlider from "./pages/Homepage/Header&Footer/Header/Slider";
 import Mainroutes from "./Mainroutes/Mainroutes";
 
