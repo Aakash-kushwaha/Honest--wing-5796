fd541ace4db7aef29019107e3803ad4cf35cb641
Prayash <prayashchhetri221@gmail.com>
2022-07-23 17:59:03 +0530

    initial update

--- src/Mainroutes/Mainroutes.jsx
+++ src/Mainroutes/Mainroutes.jsx
@@ -5,6 +5,7 @@ import Homepage from "../pages/Homepage/Homepage";
 import MainNews from "../pages/Newspage/MainNews";
 import Storypage from "../pages/Storypage/Storypage";
 import Scoredescription from "../pages/Scoredescription/Scoredescription";
+import RacismPage from './../pages/Newspage/racismpage/RacismPage';
 const Mainroutes = () => {
   return (
       
@@ -12,7 +13,7 @@ const Mainroutes = () => {
     <Route path={"/slider"} element={<SimpleSlider></SimpleSlider>}></Route>
     <Route path="/" element={<Homepage></Homepage>}></Route>
     <Route path={"/newspage"} element={<MainNews></MainNews>}></Route>
-     
+     <Route path={"/newspage/racism"} element={<RacismPage/>} />
       <Route path="/storypage/:id" element={<Storypage />} />
     </Routes>
   );
