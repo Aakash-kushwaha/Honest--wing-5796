d21abd96b4fa156627fc6c00dcb6870fb5e5af2e
Suhani1102 <sandhusuhani1102@gmail.com>
2022-07-23 15:27:53 +0530

    bug fixes and removed unwanted code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import "./App.css";
-import Homepage from "./pages/Homepage/Homepage";
-import { Box } from "@chakra-ui/react";
-import SimpleSlider from "./pages/Homepage/Header&Footer/Header/Slider";
-import Mainroutes from "./Mainroutes/Mainroutes";
-// import "./App.css";
+git;
 import Mainroutes from "./Mainroutes/Mainroutes";
 import Footer from "./pages/Homepage/Header&Footer/Footer/Footer";
 
--- src/index.js
+++ src/index.js
@@ -5,18 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { NewsProvider } from "./context/NewsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <NewsProvider>
-      <BrowserRouter>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </BrowserRouter>
-    </NewsProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
